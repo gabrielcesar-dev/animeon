b7cc5edee2b5b205535ae807e3f56f96e532a3e7
gabrielcesar-dev <142801180+gabrielcesar-dev@users.noreply.github.com>
2025-03-09 21:29:38 -0300

    feat: add variables to save default settings and localStorage keys

---
+++ src/constants/defaultPreferences.ts
@@ -0,0 +1,20 @@
+const defaultPreferences = {
+    popularity: 8196,
+    adultContent: false,
+    onMal: false,
+    format: {
+        TV: true,
+        TV_SHORT: false,
+        MOVIE: false,
+        SPECIAL: false,
+        OVA: false,
+        ONA: true,
+        MUSIC: false,
+        MANGA: false,
+        NOVEL: false,
+        ONE_SHOT: false,
+    },
+
+};
+
+export default defaultPreferences;
---
+++ src/constants/theme.ts
@@ -0,0 +1,9 @@
+const theme = {
+  colors: {
+    backgroundColor: "oklch(0.208 0.042 265.755)",
+    accentColor: "#615FFF",
+  },
+  carouselGap: 100,
+};
+
+export default theme;
