fbfd1d97667903b8396ebf2f42db2119e95f77a5
gabrielcesar-dev <142801180+gabrielcesar-dev@users.noreply.github.com>
2025-03-09 21:28:29 -0300

    feat: add card component to mark the start of each day

---
+++ src/components/DayCard.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+interface DayCardProps {
+    time: number;
+}
+
+const DayCard = ({ time }: DayCardProps) => {
+    const [userLanguege, ] = useState<string>(() => {
+        return navigator.language;
+    });
+
+    const date = new Date(time * 1000);
+    const weekDay = date.toLocaleDateString(userLanguege, {
+    weekday: 'long',
+    });
+
+
+    const dateFormatted = date.toLocaleDateString(userLanguege, {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    timeZoneName: 'short',
+    });
+
+  return (
+    <div 
+        className="absolute left-1/2 -top-20 flex justify-center items-center bg-palette-accent z-20 rounded py-4 translate-x-[-50%] ease-in-out duration-75 w-1/2
+                   outline-none focus:outline-none"
+    >
+        <time className="text-2xl text-white font-light font-poppins" dateTime={dateFormatted} title={dateFormatted}>
+            { weekDay }
+        </time>
+    </div>
+  )
+}
+
+export default DayCard
