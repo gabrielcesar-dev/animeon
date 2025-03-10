d596cd11ba16100bf687d8ee434c4261f052b81c
gabrielcesar-dev <142801180+gabrielcesar-dev@users.noreply.github.com>
2025-03-09 21:18:11 -0300

    feat: add query

---
+++ src/graphql/airingToday.ts
@@ -0,0 +1,26 @@
+import { gql } from "@apollo/client";
+
+export const GET_AIRING_TODAY = gql`
+  query GetAiringToday($start: Int!, $end: Int!) {
+    Page {
+      airingSchedules(airingAt_greater: $start, airingAt_lesser: $end) {
+        media {
+          title {
+            romaji
+            english
+          }
+          coverImage {
+            extraLarge
+            color
+          }
+          popularity
+          isAdult
+          idMal
+          id
+          format
+        }
+      airingAt
+      }
+    }
+  }
+`;
