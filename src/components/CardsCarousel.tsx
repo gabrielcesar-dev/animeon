e1e19894111817ef6b7128441a9c653dd1563ac0
gabrielcesar-dev <142801180+gabrielcesar-dev@users.noreply.github.com>
2025-03-10 18:49:06 -0300

    fix: remove refresh on resize and change keys attribute

--- src/components/CardsCarousel.tsx
+++ src/components/CardsCarousel.tsx
@@ -1,26 +1,13 @@
 import { AnimeType } from "../@types/AnimeType"
 import theme from "../constants/theme";
 import Card from "./Card";
-import { useEffect, useRef, useState } from "react";
 
 interface CardsCarouselProps {
     animeList?: AnimeType[];
 }
 
 const CardsCarousel = ({ animeList }: CardsCarouselProps) => {
-    const containerRef = useRef<HTMLDivElement>(null);
-    const [resizeCarousel, setResizeCarousel] = useState<number>(0);
     let prevDate = 0;
-
-    useEffect(() => {
-        function handleResize() {
-            setResizeCarousel((prev) => prev +  1);
-        }
-        window.addEventListener("resize", handleResize);
-
-
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
 
     return (
         <div className="absolute z-10">
@@ -28,12 +15,11 @@ const CardsCarousel = ({ animeList }: CardsCarouselProps) => {
                 id="card-carousel" 
                 className="h-screen flex flex-nowrap pl-0 overflow-hidden items-center perspective-distant" 
                 style={{ gap: `${theme.carouselGap}px` }}
-                ref={containerRef}
             >
-                {animeList && animeList.map((anime, index) => (
+                {animeList && animeList.map((anime) => (
                 <Card
                     idMal={anime.idMal}
-                    key={anime.id + "-" + index + "-" + resizeCarousel}
+                    key={anime.id}
                     src={anime.coverImage.extraLarge} 
                     alt={anime.title.english || anime.title.romaji}
                     title={anime.title.english || anime.title.romaji}
